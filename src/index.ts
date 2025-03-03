9f8ade382526b6f46f523ff452a490379f4aaa19
Nirmal Jyoti Biswas <kaizenjb@Nirmals-MacBook-Air.local>
2025-03-03 20:12:30 +0530

    fixed the code

--- src/index.ts
+++ src/index.ts
@@ -22,8 +22,8 @@ app.post('/sum', async (req, res) => {
 
     const request = await prismaClient.request.create({
         data: {
-            a: b,
-            b: a,
+            a: a,
+            b: b,
             answer: sum,
             type: "Sum"
         }
